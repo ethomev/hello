e804dd80e034ff26341b0aff50f3231758309395
Thomas Melville <thomas.melville@ericsson.com>
2017-07-27 12:45:35 +0100

    update script to use stringutil

--- hello.go
+++ hello.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/ethomev/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("hello, world\n"))
 }
 
